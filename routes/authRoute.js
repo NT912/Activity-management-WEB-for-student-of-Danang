931d5b940e0d4013ca49432be7dfa5fe9d7008fc
lamnino <lamngthbi@gmail.com>
2024-05-17 16:09:52 +0700

    fix register

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -17,63 +17,11 @@ router.get("/registerSV", authController.GET_RegisterSV);
 router.get("/registerNTC", authController.GET_RegisterNTC);
 
 router.post("/registerSV", 
-                // [
-                //     check('email').isEmail().withMessage('Please enter valid message'),
-                //     body(
-                //         'name',
-                //         'please enter name with only number and text within 5 to 30 characters'
-                //     )
-                //         .isLength({min: 5,max: 30})
-                //         .isAlphanumeric(), 
-                //     body(
-                //         'password',
-                //         'length of password from 8 to 50 character'
-                //     )
-                //         .isLength({min: 8,max: 50}),
-                //     body('confirm_password').custom((value, {req}) =>
-                //     {
-                //         if (value !== req.body.password)
-                //         {
-                //             throw new Error('password have to match!');
-                //         } else
-                //         return true;
-                //     })
-
-                // ],
                 authController.POST_RegisterSV);
-
 router.post("/registerNTC", 
-                // [
-                //     check('email').isEmail().withMessage('Please enter valid message'),
-                //     body(
-                //         'name',
-                //         'please enter name with only number and text within 5 to 30 characters'
-                //     )
-                //         .isLength({min: 5,max: 30})
-                //         .isAlphanumeric(), 
-                //     body(
-                //         'password',
-                //         'length of password from 8 to 50 character'
-                //     )
-                //         .isLength({min: 8,max: 50}),
-                //     body('confirm_password').custom((value, {req}) =>
-                //     {
-                //         if (value !== req.body.password)
-                //         {
-                //             throw new Error('password have to match!');
-                //         } else
-                //         return true;
-                //    
-                // ],
-                authController.POST_RegisterNTC);
                 
-router.post("/login",
-                    // check('email').isEmail().withMessage('Please enter valid message'),
-                    // body(
-                    //     'password',
-                    //     'length of password from 8 to 50 character'
-                    // )
-                    //     .isLength({min: 8,max: 50}), 
+                authController.POST_RegisterNTC);
+router.post("/login", 
                     authController.postLogin);
                     
 module.exports = router;
