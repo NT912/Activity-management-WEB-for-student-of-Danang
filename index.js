9c2887954833949bcdf2a65b8cd4724f08aa6b1a
lamnino <lamngthbi@gmail.com>
2024-05-29 17:40:49 +0700

    error

--- index.js
+++ index.js
@@ -46,14 +46,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/health_check', (req, res) => {
   res.send('OK');
 });
-
-/*
-    Routes
-*/
-
-const authRoutes = require('./src/routes/authRoute'); 
-
-app.use('/auth', authRoutes);
+app.use('/auth', require('./src/routes/auth'));
 app.use('/user', authMiddleware.isLoggedIn, require('./src/routes/user'));
 app.use('/activity', (req, res, next) => {
   req.session.previous_url = req.originalUrl
